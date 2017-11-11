3cd21bdd6a0882947db13f0ce42c334ac7fd592e
Tim B <tim@softwareMentor.com>
2017-11-10 20:33:19 -0600

    addtl data corrupta test

--- tests/chain.test.ts
+++ tests/chain.test.ts
@@ -7,7 +7,7 @@ describe("Construct Chain", () => {
     const chain = new Chain();
     expect(chain.blocks.length).to.equal(1);
   });
-  it("should have a hash", () => {
+  it("chain block should have a hash", () => {
     const chain = new Chain();
     expect(chain.blocks[0].hash).to.not.be.empty;
   });
@@ -29,6 +29,13 @@ describe("Validate Chain", () => {
     const block = new Block(1, chain.blocks[0].hash, chain.timestamp(), "some data");
     chain.add(block);
     chain.blocks[1].hash = "abcde";
+    expect(chain.validate()).to.be.false;
+  });
+  it("fail validate chain with bad block data", () => {
+    const chain = new Chain();
+    const block = new Block(1, chain.blocks[0].hash, chain.timestamp(), "some data");
+    chain.add(block);
+    chain.blocks[1].data = "some other data";
     expect(chain.validate()).to.be.false;
   });
 });
